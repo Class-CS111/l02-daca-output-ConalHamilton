856bd684f16f06d2ab00c38d92f6c9c409825020
Conal <122840957+ConalHamilton@users.noreply.github.com>
2025-02-02 23:54:29 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,32 @@
+// PARTNER NAME: Conal Hamilton
 // PARTNER NAME:
-// PARTNER NAME:
-// CS111 SECTION #:
-// DATE: 
+// CS111 SECTION #: 3033
+// DATE: February 2, 2025
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		//DECLARATION + INITIALIZATION SECTION
+		String surname = "Jacob";
+		String givenName = "John";
+		String category = "C09";
+		String cardNumber = "SRC0000000800";
+		String birthCountry = "Sweden";
+		String termsConditions = "None";
+		String birthMonth = "JUN";
+		char sex = 'M';
+		int uscisNumOne = 111;
+		int uscisNumTwo = 222;
+		int uscisNumThree = 333;
+		int birthDay = 20;
+		int birthYear = 1990;
+		int validFromDay = 02;
+		int validFromMonth = 01;
+		int validFromYear = 03;
+		int expireDay = 25;
+		int expireMonth = 12;
+		int expireYear = 26;
 		
 		//INPUT SECTION
 		// N/A
@@ -19,19 +38,19 @@ public class Main
 		System.out.println("║         UNITED STATES OF AMERICA");
 		System.out.println("║                                  EMPLOYMENT AUTHORIZATION CARD");
 		System.out.println("║                       Surname");
-		System.out.println("║                       ███████ ");
+		System.out.println("║                       Jacob ");
 		System.out.println("║  .----.    .----.     Given Name");
-		System.out.println("║ (   ^  \\  /  ^   )    ███████ ");
+		System.out.println("║ (   ^  \\  /  ^   )    John ");
 		System.out.println("║        |  |           USCIS#         Category   Card#");
-		System.out.println("║       _/  \\_          ███-███-███    ███        █████████████");
+		System.out.println("║       _/  \\_          111-222-333    C09        SRC0000000800");
 		System.out.println("║      (_    _)         Country of Birth");
-		System.out.println("║   ,    `--`    ,      ███████ ");
+		System.out.println("║   ,    `--`    ,      Sweden ");
 		System.out.println("║   \\'-.______.-'/      Terms and Conditions");
-		System.out.println("║    \\          /       ███████ ");
+		System.out.println("║    \\          /       None ");
 		System.out.println("║     '.--..--.'        Date of Birth   Sex");
-		System.out.println("║       `\"\"\"\"\"`         ██ ███ ████     █");
-		System.out.println("║                       Valid From:     ██/██/██");
-		System.out.println("║                       Card Expires:   ██/██/██");
+		System.out.println("║       `\"\"\"\"\"`         20 JUN 1990     M");
+		System.out.println("║                       Valid From:     01/02/03");
+		System.out.println("║                       Card Expires:   12/25/25");
 		System.out.println("║ ascii art by: jgs     NOT VALID FOR REENTRY TO U.S.");
 		System.out.println("╚══════════════════════════════════════════════════════════════════════");
 		//ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
